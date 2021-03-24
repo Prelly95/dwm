689fb86f5b2c7affe1b3393403c0213ac1cae6e0
Patrick Prell <prelly95@gmail.com>
2021-03-24 21:35:37 +1100

    setting up fonts

--- config.def.h
+++ config.def.h
@@ -5,8 +5,14 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+
+static const char *fonts[] ={	"Font Awesome 5 Brands:size=20",
+								"Font Awesome 5 Free:size=20",
+								"FontAwesome:size=20",
+								"Liberation Mono:size=18"
+							};
+static const char dmenufont[]       = "monospace:size=18";
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -44,12 +50,12 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,						KEY,	view,		{.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,			KEY,	toggleview,	{.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,				KEY,	tag,		{.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask,	KEY,	toggletag,	{.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
