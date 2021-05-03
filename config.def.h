d395208a59170dd463244a27873452bea2759071
Patrick Prell <prelly95@gmail.com>
2021-05-04 08:49:22 +1000

    changing colours

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static const char col_purp[]        = "#B85DE9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_gray3  },
+	[SchemeSel]  = { col_gray1, col_gray4,  col_gray3  },
 };
 
 /* tagging */
