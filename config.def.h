17699aa6737b887f3733f55a30a6197ed416b147
Patrick Prell <prelly95@gmail.com>
2021-06-10 14:46:35 +1000

    add window rules

--- config.def.h
+++ config.def.h
@@ -34,9 +34,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class		instance	title		tags mask		isfloating		monitor */
-	{"Gimp",		NULL,		NULL,		0,				1,				-1},
-	{"firefox",		"Devtools",	NULL,		0,				1,				-1},
+	/* class		instance	title				tags mask		isfloating		monitor */
+	{"Gimp",		NULL,		NULL,				0,				1,				-1},
+	{"firefox",		"Devtools",	NULL,				0,				1,				-1},
+	{NULL,			NULL,		"Web Inspector",	0,				1,				-1},
 };
 
 /* layout(s) */
